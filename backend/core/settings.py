8bd7d1b183435773d0392923887d89476abde670
lareef <lareef.lafir@gmail.com>
2026-04-09 11:39:14 +0530

    site url changed in settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
-#SITE_URL = 'https://omnichannel.autos'
-SITE_URL = 'http://localhost:8000'
+SITE_URL = 'https://omnichannel.autos'
+#SITE_URL = 'http://localhost:8000'
 
 # Security settings for production
 if not DEBUG:
